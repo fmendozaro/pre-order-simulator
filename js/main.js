5a8a183e7e0f3106da1b3abee8e0fff9fab229f3
Fer <fernandoza@gmail.com>
2020-11-28 18:43:08 -0600

    feat: add the update and get inventory functionality

--- js/main.js
+++ js/main.js
@@ -1,2 +1,37 @@
 "use strict";
-console.log("works");
+
+const API_URL = "https://preorder-json-srv.glitch.me/inventory";
+let options;
+
+let addCartBtn = document.querySelector("#add-to-cart");
+
+addCartBtn.addEventListener("click", e => {
+    getInventory().then(items => {
+        items.forEach(itemObj => {
+            if(itemObj.name === 'ps5'){
+                console.log(itemObj);
+                if (itemObj.quantity <= 0) {
+                    console.error("outta stock")
+                } else {
+                    itemObj.quantity--;
+                    options = {
+                        method: 'PUT',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(itemObj),
+                    };
+                    updateInventory(itemObj.id, options).then(response => console.log(response)).catch(error => console.error(error));
+                }
+            }
+        });
+    }).catch(error => console.error(error));
+});
+
+function getInventory() {
+    return fetch(API_URL).then(response => response.json())
+}
+
+function updateInventory(id, options) {
+    return fetch(`${API_URL}/${id}`, options).then(response => response.json())
+}
